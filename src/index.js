9fe3ee6fc37b7740203e79a6831b64cc9759503c
Dibyo Majumder <dibyomajumder23@gmail.com>
2019-10-25 02:55:13 +0530

    gh-pages
    integrated page feature

--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React from "react"
+import ReactDOM from "react-dom"
 
-import "./styles.css";
+import "./styles.css"
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <h1>Welcome to Dibyo's portfolio</h1>
+      <h2>Hi this is my awesome portfolio .. lets do this </h2>
     </div>
-  );
+  )
 }
 
-const rootElement = document.getElementById("root");
-ReactDOM.render(<App />, rootElement);
+const rootElement = document.getElementById("root")
+ReactDOM.render(<App />, rootElement)
